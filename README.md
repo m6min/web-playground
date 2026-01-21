793489c25686dccbf6a935693686fc0f1f45b533
m6min <88292388+m6min@users.noreply.github.com>
2026-01-21 12:46:53 +0300

    Optimize js logic and finalize responsive design

--- README.md
+++ README.md
@@ -31,3 +31,32 @@ To run these projects on your local machine, follow these steps:
 2.  Navigate to the project folder you wish to view (e.g., `portfolio-site`).
 3.  Open the `index.html` file directly in your browser.
 ---
+
+# 📟 [Terminal Portfolio](terminal-theme-portfolio/)
+
+This folder holds the source code for my personal portfolio website, designed to look and feel like a developer's command-line interface. My goal was to create an interactive experience using pure JavaScript logic and CSS layouts, avoiding any frameworks.
+
+
+* **Technologies:** HTML5, CSS3, Vanilla JavaScript (DOM Manipulation)
+* **Note:** I tried to use event listeners javascript functions.
+
+| Status | Description |
+| :--- | :--- |
+| **Responsiveness** | **Works good on big or small screens without problem.** |
+| **Features** | **Supports commands like `help`, `about`, `projects`. Try `sudo rm -rf` at your own risk :)** |
+
+(See the live site [here](https://m6min.github.io/web-playground/terminal-portfolio/)) 
+---
+
+## 🚀 Getting Started
+
+To run this project on your local machine, follow these steps:
+
+1.  Clone the repository:
+    ```bash
+    git clone [https://github.com/m6min/web-playground/terminal-portfolio.git](https://github.com/m6min/web-playground/terminal-portfolio.git)
+    ```
+
+2.  Navigate to the project folder.
+3.  Open the `index.html` file directly in your browser.
+---
--- terminal-theme-portfolio/script.js
+++ terminal-theme-portfolio/script.js
@@ -1,7 +1,73 @@
-const button_2 = document.querySelector('.b2');
-button_2.addEventListener('click', () => {
+const closeButton = document.querySelector('.b2');
+// close the page function
+closeButton.addEventListener('click', () => {
     let isConfirmed = confirm("You are closing the terminal.");
     if(isConfirmed){
         window.location.replace("https://google.com");
+    }
+});
+// command checking and answering
+const answer = (command) => {
+    // adding elements
+    const row = document.querySelector(".input").parentElement;
+    const terminal = document.querySelector(".terminal");
+    const newRow = document.createElement("div");
+    const newSpanLeft = document.createElement("span");
+    const newSpanRight = document.createElement("span");
+    newRow.classList.add("row");
+    newSpanRight.classList.add("text");
+    newSpanLeft.classList.add("text");
+    newSpanRight.textContent = command;
+    newSpanLeft.textContent = "PS C:\\Users\\visitor>";
+    terminal.insertBefore(newRow, row);
+    newRow.append(newSpanLeft, newSpanRight);
+    // create answer div
+    const answerDiv = document.createElement("div");
+    // SWITCH-CASE
+    let inputFixed = command.trim().toLowerCase();
+    switch (inputFixed) {
+        case "help":
+            answerDiv.classList.add("output-green");
+            answerDiv.innerHTML = `Commands: <br> 
+            'about' <br>
+            'contact'<br>
+            'projects'<br>
+            'sudo rm -rf'
+            `;
+            break;
+        case "about":
+            answerDiv.classList.add("output-green");
+            answerDiv.innerHTML = `Hi there, I'm Mümin!
+             I'm studying Computer Engineering at Yalova University. And yes, I mostly do web developing.`;
+            break;
+        case "contact":
+            answerDiv.classList.add("output-green");
+            answerDiv.innerHTML = `<a class="links" href="https://instagram.com/" target="_blank">Instagram (Not added Yet)</a><br>
+            <a class="links" href="https://x.com/" target="_blank">X (Not added Yet)</a><br>
+            <a class="links" href="mailto:muminflz@gmail.com" target="_blank">E-Mail</a>
+            `;
+            break;
+        case "projects": 
+            answerDiv.classList.add("output-green");
+            answerDiv.innerHTML = `For more projects: <a class="links" href="https://github.com/m6min/">Github</a>`
+            break;
+        case "sudo rm -rf":
+            answerDiv.classList.add("output-red");
+            answerDiv.innerHTML = `Permission denied: You are not the 'root' user. Nice try tho :)`;
+            break; 
+        default:
+            answerDiv.classList.add("output-red");
+            answerDiv.textContent = `'${command}' is not recognized as an internal or external command, operable program or batch file.`;
+            break;
+    }
+    terminal.insertBefore(answerDiv, row);
+    window.scrollTo(0, document.body.scrollHeight);
+}
+let input = document.querySelector('.input');
+input.addEventListener("keyup", (e) => {
+    if(e.key === "Enter"){
+        const command = input.value;
+        answer(command);
+        input.value = '';
     }
 });
